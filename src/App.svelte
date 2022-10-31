ddbc4ce950809d75d62fb0db144557d7689af1c0
KARTHICK\hp <karthikeyanjeyaveeran@gmail.com>
2022-10-31 15:00:18 +0530

    required modules installed

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let name
+</script>
+
+<main>
+  <h1 class=" p-2 text-center text-3xl text-red-400">Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+</main>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
